a02ab2b9437ed7a6d5a08f2cf864a5fc61c116e5
Ilya Kiyanov <ikiyanov@protonmail.com>
2018-11-26 02:23:35 +0300

    Adds some jQuery methods in index.js and adds style for more-info page.

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-let pageIndex = document.getElementById("page_index"),
+var pageIndex = document.getElementById("page_index"),
     pageSponsor = document.getElementById("page_sponsor"),
     pageRunner = document.getElementById("page_runner"),
-    pageInfo = document.getElementById("page_info"),
+    pageInfo = $("#page_more_info"),
     pageLogin = document.getElementById("page_login");
 
-let btnBack = document.getElementById("btn_back"),
-    btnRunner = document.getElementById("btn_runner"),
-    btnInfo = document.getElementById("btn_info"),
+var btnRunner = document.getElementById("btn_runner"),
+    btnInfo = $("#btn_info"),
     btnSponsor = document.getElementById("btn_sponsor");
 
-function switchPages() {
-    if (pageIndex.style.display !== "none") {
-        btnBack.style.display = "none";
-    }
+var main = $("main");
+var pages = main.children(".page");
 
-    if (pageSponsor.style.display === "block") {
-        btnBack.style.display = "block";
-    }
-}
+$(function () {
+	$("#btn_back").on("click", function () {
+		pages.hide();
+		$("#page_index").show();
+	});
+
+	btnInfo.on("click", function () {
+		pages.hide();
+		pageInfo.show();
+	});
+});
 
 btnSponsor.addEventListener("click", evt => {
     pageIndex.style.display = "none";
@@ -28,13 +32,6 @@ btnRunner.addEventListener("click", evt => {
     pageIndex.style.display = "none";
     pageRunner.style.display = "block";
 });
-
-btnInfo.addEventListener("click", evt => {
-    pageIndex.style.display = "none";
-    pageInfo.style.display = "block";
-});
-
-switchPages();
 
 $(function () {
     var mysql = require('mysql');
